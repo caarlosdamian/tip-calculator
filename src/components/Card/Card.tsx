f4c2406c564333ebdf9181b78e92380bd8c40e66
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-06-09 23:46:35 -0500

    finish left part of card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../Input/Input";
 import dollar from "../../images/icon-dollar.svg";
+import person from "../../images/icon-person.svg";
 import "./Card.scss";
 import { Button } from "../Button/Button";
 import { buttonData } from "../../utils/data";
 
 export const Card = () => {
+  const [people, setPeople] = useState<number>(2);
+  const [tip, setTip] = useState<number>();
   return (
     <div className="card__container">
       <div className="top__wrapper">
@@ -18,8 +21,20 @@ export const Card = () => {
           {buttonData.map((item) => (
             <Button label={item.label} />
           ))}
-            <input type="number" className="middle__grid-input" placeholder="Custom"/>
+          <input
+            type="number"
+            className="middle__grid-input"
+            placeholder="Custom"
+          />
         </div>
+      </div>
+      <div className="bottom__wrapper">
+        <div className="bottom_wrapper-container">
+          <span className="card__span">Number of People</span>
+          {people === 0 && <span className="card__span--error">Can’t be zero</span>}
+        </div>
+
+        <Input icon={person} people={people} clasName="red-input" />
       </div>
     </div>
   );
